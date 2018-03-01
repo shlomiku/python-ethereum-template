59f0690ba4ce13f1b2938f96be4e6f07f3aee355
Shlomi K <shlomik@gmail.com>
2018-03-01 17:46:47 +0200

    creating the ref copy of the deployed rinkeby contract
    this will be used to communicate with the deployed contract

---
+++ lottery-fe-react/src/lottery.js
@@ -0,0 +1,95 @@
+import web3 from './web3';
+
+// the address of the deployed contract, received from running deploy.js
+const address = '0xbD31c969F2983f6a4c668d9B2cDbf992c2eC1FCF';
+
+// the result of the deploy.js script. I used this for pretty print json: http://jsonprettyprint.com/
+const abi = [
+  {
+    "constant": true,
+    "inputs": [
+
+    ],
+    "name": "manager",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+
+    ],
+    "name": "pickWinner",
+    "outputs": [
+
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+
+    ],
+    "name": "getPlayers",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address[]"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+
+    ],
+    "name": "enter",
+    "outputs": [
+
+    ],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "players",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  }
+];
+
+export default new web3.eth.Contract(abi, address);
