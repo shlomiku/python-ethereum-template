0c481915faf5076de30a4a940ce095a46dfe59f3
Shlomi K <shlomik@gmail.com>
2018-03-01 17:02:45 +0200

    deploying to Rinkeby using infura

---
+++ solidity/deploy/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'general this teach tiger smoke country bachelor crawl vendor donor inhale flower',
+  'https://rinkeby.infura.io/DP4o7SKdU7csXkZ8pxGa'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(interface);
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
