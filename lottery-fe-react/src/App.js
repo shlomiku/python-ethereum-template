2f782f8609e6fc03c6ae1956784f4bd265015c54
Shlomi K <shlomik@gmail.com>
2018-03-04 17:52:31 +0200

    setting balance and players

--- lottery-fe-react/src/App.js
+++ lottery-fe-react/src/App.js
@@ -6,14 +6,19 @@ import lottery from './lottery';
 
 
 class App extends Component {
-    state = { manager: ''};
+    state = {
+        manager: '',
+        players: [],
+        balance: ''
+    };
 
     // when the component is loaded to the page
     async componentDidMount() {
         // note: we don't need to specify the 'from' field inside the call method because
         const manager = await lottery.methods.manager().call();
-        this.setState({manager});
-
+        const players = await lottery.methods.getPlayers().call();
+        const balance = await web3.eth.getBalance(lottery.options.address);
+        this.setState({manager, players, balance});
     }
 
     render() {
