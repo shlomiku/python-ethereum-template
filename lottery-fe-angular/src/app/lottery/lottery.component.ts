7d6d3c8598213f36be9b1cb9c77e404ef8833f0e
Shlomi K <shlomik@gmail.com>
2018-03-04 20:05:37 +0200

    implemented angular basic usage of lottery interface

--- lottery-fe-angular/src/app/lottery/lottery.component.ts
+++ lottery-fe-angular/src/app/lottery/lottery.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import lottery from '../lottery-interface'
+import lottery from '../lottery-interface';
+import web3 from '../web3';
 
 @Component({
   selector: 'lottery-game',
   template: `
-    <p>
-      {{manager}}
-    </p>
+    <div>
+      <p>the game manager: {{manager}}</p>
+      <p>number of players: {{ players?.length }}</p>
+      <p>prize amount: {{ balance }}</p>
+    </div>
   `,
   styles: []
 })
 export class LotteryComponent implements OnInit {
   manager: any;
-  constructor() { }
+  players: any[];
+  balance: any;
+
+  constructor() {}
 
   async ngOnInit() {
     this.manager = await lottery.methods.manager().call();
+    this.players = await lottery.methods.getPlayers().call();
+    this.balance = await web3.eth.getBalance(lottery.options.address);
+    this.balance = web3.utils.fromWei(this.balance, 'ether');
   }
 
 }
