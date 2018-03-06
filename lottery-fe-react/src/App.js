d072335b65549f6767eeb25f178c46bb97ba66b2
Shlomi K <shlomik@gmail.com>
2018-03-06 15:35:01 +0200

    completed react implementation with these final features:
    1. select winner
    2. only allow the manager to select winner
    3. idle operation to update contract data
    4. display the previous winner

--- lottery-fe-react/src/App.js
+++ lottery-fe-react/src/App.js
@@ -11,37 +11,83 @@ class App extends Component {
         players: [],
         balance: '',
         value: '',
-        message: ''
+        message: '',
+        isManagerAccount: false,
+        previousWinner: ''
     };
+
+    // gets the current metamask ACTIVE account.
+    async getCurrentAccount() {
+        // it will always return a list of 1 active account.
+        const accounts = await web3.eth.getAccounts();
+        return accounts[0];
+    }
+
+    async getLatestContractData() {
+        const players = await lottery.methods.getPlayers().call();
+        const balance = await web3.eth.getBalance(lottery.options.address);
+        this.setState({players, balance});
+    }
 
     // when the component is loaded to the page
     async componentDidMount() {
         // note: we don't need to specify the 'from' field inside the call method because
         const manager = await lottery.methods.manager().call();
-        const players = await lottery.methods.getPlayers().call();
-        const balance = await web3.eth.getBalance(lottery.options.address);
-        this.setState({manager, players, balance});
+        const previousWinner = await lottery.methods.getPreviousWinner().call();
+        await this.getLatestContractData();
+        this.setState({manager, previousWinner});
     }
+
+
     onSubmit = async (event) => {
         event.preventDefault(); // prevent the default empty value
-        const accounts = await web3.eth.getAccounts();
-        console.log(accounts);
         this.setState({message: "Waiting for transaction to complete"});
         await lottery.methods.enter().send({
-            from: accounts[0],
+            from: await this.getCurrentAccount(),
             value: web3.utils.toWei(this.state.value, 'ether')
         });
         this.setState({message: "Transaction complete, welcome to the game"});
+        await this.getLatestContractData();
 
     };
 
+    selectWinner = async () => {
+        if (this.state.players.length) {
+            this.setState({message: "Wait for winner to be selected"});
+            await lottery.methods.pickWinner().send({from: await this.getCurrentAccount()});
+            await this.getLatestContractData();
+            this.setState({message: "Winner selected. lottery reset"});
+            const previousWinner = await lottery.methods.getPreviousWinner().call();
+            this.setState({previousWinner});
+        }
+    };
+
+    async checkIfManagerAccount() {
+        const manager = await lottery.methods.manager().call();
+        if (await this.getCurrentAccount() === manager) {
+            if (this.state.isManagerAccount === false) {
+                this.setState({isManagerAccount: true});
+            }
+        } else {
+            if (this.state.isManagerAccount === true) {
+                this.setState({isManagerAccount: false});
+            }
+        }
+        setTimeout(() => {
+            this.checkIfManagerAccount();
+        }, 500);
+    }
+
     render() {
+        this.checkIfManagerAccount();
         return (
             <div>
                 <h2>Welcome to the Lottery game</h2>
                 <p> our manager is: {this.state.manager} </p>
                 <p> number of players: {this.state.players.length} </p>
                 <p> lottery amount: {web3.utils.fromWei(this.state.balance, 'ether')} ether </p>
+                <button onClick={this.selectWinner} style={this.state.isManagerAccount ? {} : { display: 'none' }}>Select Winner</button>
+                <p> previous winner: {this.state.previousWinner ? this.state.previousWinner: ''} </p>
                 <hr/>
                 <form onSubmit={this.onSubmit}>
                     <h4>Want to play?</h4>
